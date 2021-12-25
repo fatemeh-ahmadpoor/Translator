3321d9c1a9a38998cce7b89c2aed9c63f908eb19
fatemeh-ahmadpoor <fatemehstar2021@gmail.com>
2021-12-25 06:57:22 -0800

    splash screen

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MediaPlayer splashSound;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        splashSound = MediaPlayer.create(this, R.raw.sound2);
+        splashSound.start();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },2000);
     }
 }
